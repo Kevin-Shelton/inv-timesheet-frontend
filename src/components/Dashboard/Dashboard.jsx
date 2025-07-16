d57f0c8ebd9ee1aa37ca2179cc4e8c6865826f7c
Kevin-Shelton <198251046+Kevin-Shelton@users.noreply.github.com>
2025-07-15 19:10:14 -0600

    dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -6,45 +6,45 @@ import ActivityRing from './ActivityRing';
 import WeeklyChart from './WeeklyChart';
 import WhoIsInOutPanel from './WhoIsInOutPanel';
 import './Dashboard.css';
+import './DashboardNamespaced.css';
 
 const Dashboard = () => {
   return (
-    <div className="dashboard-container">
-      <div className="dashboard-row">
-        <div className="dashboard-col welcome">
-          <WelcomeCard />
+    <div className="dashboard-page with-sidebar">
+      <div className="dashboard-main">
+        <div className="dashboard-row">
+          <div className="dashboard-col welcome">
+            <WelcomeCard />
+          </div>
+          <div className="dashboard-col holidays">
+            <HolidaySection />
+          </div>
         </div>
-        <div className="dashboard-col holidays">
-          <HolidaySection />
+
+        <div className="dashboard-row">
+          <div className="dashboard-col wide">
+            <WeeklyChart />
+          </div>
+        </div>
+
+        <div className="dashboard-row">
+          <div className="dashboard-col activity">
+            <ActivityRing />
+          </div>
+        </div>
+
+        <div className="dashboard-row">
+          <div className="dashboard-col time">
+            <CurrentTime />
+          </div>
         </div>
       </div>
 
-      <div className="dashboard-row">
-        <div className="dashboard-col wide">
-          <WeeklyChart />
-        </div>
-      </div>
-
-      <div className="dashboard-row">
-        <div className="dashboard-col activity">
-          <ActivityRing />
-        </div>
-      </div>
-
-      <div className="dashboard-row">
-        <div className="dashboard-col time">
-          <CurrentTime />
-        </div>
-      </div>
-
-      <div className="dashboard-row">
-        <div className="dashboard-col whoisinout-panel">
-          <WhoIsInOutPanel />
-        </div>
+      <div className="dashboard-sidebar">
+        <WhoIsInOutPanel />
       </div>
     </div>
   );
 };
 
 export default Dashboard;
-
